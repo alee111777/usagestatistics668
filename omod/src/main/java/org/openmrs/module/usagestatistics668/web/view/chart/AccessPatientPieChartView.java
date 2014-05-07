9503165be7c51bc09f2505823e72903e4cf13c4c
Ye Cheng <ycheng82@gmail.com>
2014-05-07 16:27:50 -0700

    add pie chart for patient table

--- omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessPatientPieChartView.java
+++ omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessPatientPieChartView.java
@@ -34,11 +34,11 @@ import org.openmrs.module.usagestatistics668.util.StatsUtils;
 /**
  * View to render hour/day usage data as a chart image
  */
-public class AccessPatientChartView extends AbstractChartView {
+public class AccessPatientPieChartView extends AbstractChartView {
 
     @Override
     protected JFreeChart createChart(Map<String, Object> model, HttpServletRequest request) {
-        System.out.println("---------create chart--------------");
+        System.out.println("---------create pie chart--------------");
 
         AccessPatientService svc = Context.getService(AccessPatientService.class);
 
@@ -65,27 +65,64 @@ public class AccessPatientChartView extends AbstractChartView {
             seriesView = ContextProvider.getMessage("usagestatistics668.summary.unvoided");
         }
 
-        String yAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.count");
-        String xAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.patient");
+        String title = ContextProvider.getMessage("usagestatistics668.summary.titile.patient");
         //String seriesView = ContextProvider.getMessage("usagestatistics668.summary.view");
 
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        DefaultPieDataset dataset = new DefaultPieDataset();
         for (int c = 0; c < data.size(); c++) {
-            dataset.addValue(count[c], seriesView, (categories != null) ? categories[c] : (c + ""));
+            dataset.setValue(String.valueOf(data.get(c)[0]), ((BigInteger) data.get(c)[1]).intValue());
         }
-
-        JFreeChart chart = ChartFactory.createBarChart(
-                null, // Chart title
-                xAxisLabel, // Domain axis label
-                yAxisLabel, // Range axis label
-                dataset, // Data
-                PlotOrientation.VERTICAL, // Orientation
-                true, // Include legend
-                false, // Tooltips?
-                false // URLs?
+        
+        JFreeChart chart = ChartFactory.createPieChart(
+                title, // chart title
+                dataset, // data
+                false, // no legend
+                true, // tooltips
+                false // no URL generation
         );
 
+        /*
+        // set a custom background for the chart
+        chart.setBackgroundPaint(new GradientPaint(new Point(0, 0),
+                new Color(20, 20, 20), new Point(400, 200), Color.DARK_GRAY));
+
+        // customise the title position and font
+        TextTitle t = chart.getTitle();
+        t.setHorizontalAlignment(HorizontalAlignment.LEFT);
+        t.setPaint(new Color(240, 240, 240));
+        t.setFont(new Font("Arial", Font.BOLD, 26));
+
+        PiePlot plot = (PiePlot) chart.getPlot();
+        plot.setBackgroundPaint(null);
+        plot.setInteriorGap(0.04);
+        plot.setOutlineVisible(false);
+
+        // use gradients and white borders for the section colours
+        plot.setSectionPaint("Others", createGradientPaint(new Color(200, 200, 255), Color.BLUE));
+        plot.setSectionPaint("Samsung", createGradientPaint(new Color(255, 200, 200), Color.RED));
+        plot.setSectionPaint("Apple", createGradientPaint(new Color(200, 255, 200), Color.GREEN));
+        plot.setSectionPaint("Nokia", createGradientPaint(new Color(200, 255, 200), Color.YELLOW));
+        plot.setBaseSectionOutlinePaint(Color.WHITE);
+        plot.setSectionOutlinesVisible(true);
+        plot.setBaseSectionOutlineStroke(new BasicStroke(2.0f));
+
+        // customise the section label appearance
+        plot.setLabelFont(new Font("Courier New", Font.BOLD, 20));
+        plot.setLabelLinkPaint(Color.WHITE);
+        plot.setLabelLinkStroke(new BasicStroke(2.0f));
+        plot.setLabelOutlineStroke(null);
+        plot.setLabelPaint(Color.WHITE);
+        plot.setLabelBackgroundPaint(null);
+
+        // add a subtitle giving the data source
+        TextTitle source = new TextTitle("Source: http://www.bbc.co.uk/news/business-15489523",
+                new Font("Courier New", Font.PLAIN, 12));
+        source.setPaint(Color.WHITE);
+        source.setPosition(RectangleEdge.BOTTOM);
+        source.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        chart.addSubtitle(source);*/
         return chart;
     }
 
 }
+
--- omod/src/main/webapp/patientChart.jsp
+++ omod/src/main/webapp/patientChart.jsp
@@ -42,4 +42,10 @@
 <div class="box" style="text-align: center">
 	<img src="chart.htm?chart=patient&amp;from=${privacy:formatDate(from)}&amp;until=${privacy:formatDate(until)}&amp;usageFilter=${usageFilter.ordinal}&amp;width=700&amp;height=200" width="700" height="200" />
 </div>
+<b class="boxHeader">
+	<spring:message code="usagestatistics668.summary.titile.patient"/>
+</b>
+<div class="box" style="text-align: center">
+	<img src="piechart.htm?chart=patient&amp;from=${privacy:formatDate(from)}&amp;until=${privacy:formatDate(until)}&amp;usageFilter=${usageFilter.ordinal}&amp;width=700&amp;height=200" width="700" height="200" />
+</div>
 <%@ include file="/WEB-INF/template/footer.jsp"%>
