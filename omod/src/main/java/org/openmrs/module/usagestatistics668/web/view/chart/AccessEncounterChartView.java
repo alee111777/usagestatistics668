9d1e7752be8b115c53ada12e3f56bd0435ea1389
Ye Cheng <ycheng82@gmail.com>
2014-05-07 22:00:46 -0700

    fix legend

--- omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessEncounterChartView.java
+++ omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessEncounterChartView.java
@@ -1,15 +1,13 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ * The contents of this file are subject to the OpenMRS Public License Version
+ * 1.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://license.openmrs.org
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
  *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ * Copyright (C) OpenMRS, LLC. All Rights Reserved.
  */
 package org.openmrs.module.usagestatistics668.web.view.chart;
 
@@ -25,6 +23,7 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.usagestatistics668.AccessEncounterService;
+import org.openmrs.module.usagestatistics668.ActionCriteria;
 import org.openmrs.module.usagestatistics668.util.ContextProvider;
 
 /**
@@ -32,44 +31,54 @@ import org.openmrs.module.usagestatistics668.util.ContextProvider;
  */
 public class AccessEncounterChartView extends AbstractChartView {
 
-	@Override
-	protected JFreeChart createChart(Map<String, Object> model, HttpServletRequest request) {
-            System.out.println("---------create chart--------------");
-		
-		AccessEncounterService svc = Context.getService(AccessEncounterService.class);
+    @Override
+    protected JFreeChart createChart(Map<String, Object> model, HttpServletRequest request) {
+        System.out.println("---------create chart--------------");
 
-                //Date monthAgo = StatsUtils.addDaysToDate(null, -30);
-		List<Object[]> data = svc.getMostViewedEncounter(getFromDate(), getUntilDate(),getUsageFilter(),getMaxResults());
-                //List<Object[]> data = svc.getMostViewedPatient(monthAgo, 2);
-		String[] categories = new String[data.size()] ;
-                int[] count = new int[data.size()];
-                for (int i =0;i < categories.length;i++){
-                    categories[i] = String.valueOf(data.get(i)[0]);
-                    count[i] = ((BigInteger)data.get(i)[1]).intValue();
-                }
+        AccessEncounterService svc = Context.getService(AccessEncounterService.class);
 
-		String yAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.count");
-		String xAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.encounter");
-                String seriesView = ContextProvider.getMessage("usagestatistics668.summary.view");
+        //Date monthAgo = StatsUtils.addDaysToDate(null, -30);
+        List<Object[]> data = svc.getMostViewedEncounter(getFromDate(), getUntilDate(), getUsageFilter(), getMaxResults());
+        //List<Object[]> data = svc.getMostViewedPatient(monthAgo, 2);
+        String[] categories = new String[data.size()];
+        int[] count = new int[data.size()];
+        for (int i = 0; i < categories.length; i++) {
+            categories[i] = String.valueOf(data.get(i)[0]);
+            count[i] = ((BigInteger) data.get(i)[1]).intValue();
+        }
 
-                DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-                for (int c = 0; c < data.size(); c++) {
-                       dataset.addValue(count[c], seriesView, (categories != null) ? categories[c] : (c + ""));
-                }
-        
-                JFreeChart chart = ChartFactory.createBarChart(
-                        null,						// Chart title
-                        xAxisLabel,					// Domain axis label
-                        yAxisLabel,					// Range axis label
-                        dataset,					// Data
-                        PlotOrientation.VERTICAL,	// Orientation
-                        true,						// Include legend
-                        false,						// Tooltips?
-                        false						// URLs?
-                );
+        String yAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.count");
+        String xAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.encounter");
+        String seriesView = ContextProvider.getMessage("usagestatistics668.summary.any");
+        if (getUsageFilter() == ActionCriteria.CREATED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.created");
+        } else if (getUsageFilter() == ActionCriteria.UPDATED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.updated");
+        } else if (getUsageFilter() == ActionCriteria.VIEWED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.viewed");
+        } else if (getUsageFilter() == ActionCriteria.VOIDED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.voided");
+        } else if (getUsageFilter() == ActionCriteria.UNVOIDED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.unvoided");
+        }
 
-                return chart;
-                }
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int c = 0; c < data.size(); c++) {
+            dataset.addValue(count[c], seriesView, (categories != null) ? categories[c] : (c + ""));
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                null, // Chart title
+                xAxisLabel, // Domain axis label
+                yAxisLabel, // Range axis label
+                dataset, // Data
+                PlotOrientation.VERTICAL, // Orientation
+                true, // Include legend
+                false, // Tooltips?
+                false // URLs?
+        );
+
+        return chart;
+    }
 
 }
-
--- omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessVisitChartView.java
+++ omod/src/main/java/org/openmrs/module/usagestatistics668/web/view/chart/AccessVisitChartView.java
@@ -1,15 +1,13 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ * The contents of this file are subject to the OpenMRS Public License Version
+ * 1.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://license.openmrs.org
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
  *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ * Copyright (C) OpenMRS, LLC. All Rights Reserved.
  */
 package org.openmrs.module.usagestatistics668.web.view.chart;
 
@@ -27,6 +25,7 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.openmrs.api.context.Context;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.openmrs.module.usagestatistics668.AccessVisitService;
+import org.openmrs.module.usagestatistics668.ActionCriteria;
 import org.openmrs.module.usagestatistics668.util.ContextProvider;
 import org.openmrs.module.usagestatistics668.util.StatsUtils;
 
@@ -35,44 +34,54 @@ import org.openmrs.module.usagestatistics668.util.StatsUtils;
  */
 public class AccessVisitChartView extends AbstractChartView {
 
-	@Override
-	protected JFreeChart createChart(Map<String, Object> model, HttpServletRequest request) {
-            System.out.println("---------create chart--------------");
-		
-		AccessVisitService svc = Context.getService(AccessVisitService.class);
+    @Override
+    protected JFreeChart createChart(Map<String, Object> model, HttpServletRequest request) {
+        System.out.println("---------create chart--------------");
 
-                //Date monthAgo = StatsUtils.addDaysToDate(null, -30);
-		List<Object[]> data = svc.getMostViewedVisit(getFromDate(), getUntilDate(),getUsageFilter(),getMaxResults());
-                //List<Object[]> data = svc.getMostViewedPatient(monthAgo, 2);
-		String[] categories = new String[data.size()] ;
-                int[] count = new int[data.size()];
-                for (int i =0;i < categories.length;i++){
-                    categories[i] = String.valueOf(data.get(i)[0]);
-                    count[i] = ((BigInteger)data.get(i)[1]).intValue();
-                }
+        AccessVisitService svc = Context.getService(AccessVisitService.class);
 
-		String yAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.count");
-		String xAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.visit");
-                String seriesView = ContextProvider.getMessage("usagestatistics668.summary.view");
+        //Date monthAgo = StatsUtils.addDaysToDate(null, -30);
+        List<Object[]> data = svc.getMostViewedVisit(getFromDate(), getUntilDate(), getUsageFilter(), getMaxResults());
+        //List<Object[]> data = svc.getMostViewedPatient(monthAgo, 2);
+        String[] categories = new String[data.size()];
+        int[] count = new int[data.size()];
+        for (int i = 0; i < categories.length; i++) {
+            categories[i] = String.valueOf(data.get(i)[0]);
+            count[i] = ((BigInteger) data.get(i)[1]).intValue();
+        }
 
-                DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-                for (int c = 0; c < data.size(); c++) {
-                       dataset.addValue(count[c], seriesView, (categories != null) ? categories[c] : (c + ""));
-                }
-        
-                JFreeChart chart = ChartFactory.createBarChart(
-                        null,						// Chart title
-                        xAxisLabel,					// Domain axis label
-                        yAxisLabel,					// Range axis label
-                        dataset,					// Data
-                        PlotOrientation.VERTICAL,	// Orientation
-                        true,						// Include legend
-                        false,						// Tooltips?
-                        false						// URLs?
-                );
+        String yAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.count");
+        String xAxisLabel = ContextProvider.getMessage("usagestatistics668.summary.visit");
+        String seriesView = ContextProvider.getMessage("usagestatistics668.summary.any");
+        if (getUsageFilter() == ActionCriteria.CREATED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.created");
+        } else if (getUsageFilter() == ActionCriteria.UPDATED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.updated");
+        } else if (getUsageFilter() == ActionCriteria.VIEWED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.viewed");
+        } else if (getUsageFilter() == ActionCriteria.VOIDED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.voided");
+        } else if (getUsageFilter() == ActionCriteria.UNVOIDED) {
+            seriesView = ContextProvider.getMessage("usagestatistics668.summary.unvoided");
+        }
 
-                return chart;
-                }
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int c = 0; c < data.size(); c++) {
+            dataset.addValue(count[c], seriesView, (categories != null) ? categories[c] : (c + ""));
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                null, // Chart title
+                xAxisLabel, // Domain axis label
+                yAxisLabel, // Range axis label
+                dataset, // Data
+                PlotOrientation.VERTICAL, // Orientation
+                true, // Include legend
+                false, // Tooltips?
+                false // URLs?
+        );
+
+        return chart;
+    }
 
 }
-
